f764c12efac4fc6dd48f90008d33f090b17fa1d3
Dung <vuquocdung204@gmail.com>
2026-05-12 22:25:34 +0700

    fix highlight system, add Toony  Colors Pro

--- Assets/00_Game/Core/LobbyScene/Highlight/HighlightSystem.cs
+++ Assets/00_Game/Core/LobbyScene/Highlight/HighlightSystem.cs
@@ -1,49 +1,65 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HighlightSystem : MonoBehaviour
 {
   public static HighlightSystem Instance { get; private set; }
   [SerializeField] int highlightLayerIndex = 6;
-  GameObject _current;
-  int[] _originalLayers;
+
+  private Dictionary<Renderer, int> _originalLayers = new Dictionary<Renderer, int>();
+  private List<GameObject> _currentTargets = new List<GameObject>();
 
   public void Init()
   {
     Instance = this;
   }
 
-  void OnDestroy()
-  {
-    Clear();
-  }
-
   public void Highlight(GameObject target)
   {
+    if (target == null) return;
+    Highlight(new List<GameObject> { target });
+  }
+
+  public void Highlight(List<GameObject> targets)
+  {
     Clear();
-    _current = target;
-    var renderers = target.GetComponentsInChildren<Renderer>(true);
-    _originalLayers = new int[renderers.Length];
-    for (int i = 0; i < renderers.Length; i++)
+
+    if (targets == null || targets.Count == 0) return;
+
+    _currentTargets.AddRange(targets);
+
+    foreach (var target in targets)
     {
-      _originalLayers[i] = renderers[i].gameObject.layer;
-      renderers[i].gameObject.layer = highlightLayerIndex;
+      if (target == null) continue;
+
+      var renderers = target.GetComponentsInChildren<Renderer>(true);
+      foreach (var renderer in renderers)
+      {
+        if (!_originalLayers.ContainsKey(renderer))
+        {
+          _originalLayers[renderer] = renderer.gameObject.layer;
+        }
+
+        renderer.gameObject.layer = highlightLayerIndex;
+      }
     }
+
     HighlightRendererFeature.IsActive = true;
   }
 
   public void Clear()
   {
-    if (_current != null)
+    foreach (var kvp in _originalLayers)
     {
-      var renderers = _current.GetComponentsInChildren<Renderer>(true);
-      for (int i = 0; i < renderers.Length && i < _originalLayers.Length; i++)
-        renderers[i].gameObject.layer = _originalLayers[i];
-      _current = null;
-      _originalLayers = null;
+      if (kvp.Key != null)
+      {
+        kvp.Key.gameObject.layer = kvp.Value;
+      }
     }
+
+    _originalLayers.Clear();
+    _currentTargets.Clear();
     HighlightRendererFeature.IsActive = false;
   }
-
-
 }
